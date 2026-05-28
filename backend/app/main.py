fc10dfac2236b28c82e6b88dc306b0d77ddceb0f
8112861094anuj <139586060+8112861094anuj@users.noreply.github.com>
2026-05-29 02:14:06 +0530

    updated bacjend

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,16 +2,14 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from app.database import engine, Base
-from fastapi.middleware.cors import CORSMiddleware
-from app.models import User, Expense
+
+from app.models.user import User
+from app.models.expense import Expense
 
 from app.routes.user import router as user_router
 from app.routes.expense import router as expense_router
 
-try:
-    print("Database connected successfully")
-except Exception as e:
-    print(f"Database connection failed: {e}")
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
@@ -19,7 +17,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "https://expense-tracker-vert-seven-56.vercel.app"
+        "https://expense-tracker-vert-seven-56.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
@@ -29,11 +27,9 @@ app.add_middleware(
 app.include_router(user_router)
 app.include_router(expense_router)
 
+
 @app.get("/")
 def root():
-    return {"message": "Expense Tracker API Running"}
-
-
-
-
-
+    return {
+        "message": "Expense Tracker API Running"
+    }
